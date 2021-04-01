194b6064cd902d7fa3c247dcd8b5e6c563edde3c
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2021-04-01 09:05:29 +0100

    Fix remaining errors

--- crates/bevy_ecs/src/lib.rs
+++ crates/bevy_ecs/src/lib.rs
@@ -1,25 +1,26 @@
-use std::ops::{Index, IndexMut};
+use std::{
+    convert::{TryFrom, TryInto},
+    ops::{Index, IndexMut},
+};
 
 #[derive(Default)]
 pub struct World<Component> {
     data: Vec<Component>,
 }
 
-pub struct Entity {
-    index: u64,
-}
+pub struct Entity(u64);
 
 impl<C> Index<Entity> for World<C> {
     type Output = C;
 
     fn index(&self, index: Entity) -> &Self::Output {
-        &self.data[index.0]
+        &self.data[usize::try_from(index.0).unwrap()]
     }
 }
 
 impl<C> IndexMut<Entity> for World<C> {
     fn index_mut(&mut self, index: Entity) -> &mut Self::Output {
-        &mut self.data[index.0]
+        &mut self.data[usize::try_from(index.0).unwrap()]
     }
 }
 
@@ -27,7 +28,7 @@ impl<C> World<C> {
     pub fn add(&mut self, value: C) -> Entity {
         let idx = self.data.len();
         self.data.push(value);
-        Entity(idx)
+        Entity(idx.try_into().unwrap())
     }
 
     // TODO: Supporting removing data from the world
@@ -38,12 +39,12 @@ pub struct Schedule<Component> {
     systems: Vec<Box<dyn System<Component>>>,
 }
 
-impl<T> Schedule<W> {
-    pub fn with_system<T: System>(self, it: T) -> Self {
+impl<W> Schedule<W> {
+    pub fn with_system<T: System<W>>(mut self, it: T) -> Self {
         self.add_system(it);
         self
     }
-    pub fn add_system<T: System>(&mut self, it: T) {
+    pub fn add_system<T: System<W>>(&mut self, it: T) {
         self.systems.push(Box::new(it))
     }
 }
@@ -52,7 +53,7 @@ pub trait System<W>: 'static {
     fn run(&mut self, world: &mut World<W>);
 }
 
-impl<T> System for T
+impl<T, W> System<W> for T
 where
     T: FnMut(&mut World<W>) + 'static,
 {
